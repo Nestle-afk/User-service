21feacd22f78647e55e3108ce2501f1273163a8b
Nestle-afk <nastassya04@gmail.com>
2025-10-30 16:51:46 +0300

    fix)))))))))))

--- src/test/java/com/innowise/userservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/innowise/userservice/integration/AbstractIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.innowise.userservice.integration;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,19 +13,17 @@ import org.springframework.test.context.DynamicPropertySource;
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
-    static final GenericContainer<?> redis =
+    private static final GenericContainer<?> REDIS =
             new GenericContainer<>(DockerImageName.parse("redis:7-alpine")).withExposedPorts(6379);
 
-    @BeforeAll
-    void startRedis() { redis.start(); }
-
-    @AfterAll
-    void stopRedis() { redis.stop(); }
+    static {
+        REDIS.start();
+    }
 
     @DynamicPropertySource
     static void redisProps(DynamicPropertyRegistry r) {
-        r.add("spring.data.redis.host", redis::getHost);
-        r.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+        r.add("spring.data.redis.host", REDIS::getHost);
+        r.add("spring.data.redis.port", () -> REDIS.getMappedPort(6379));
         r.add("spring.data.redis.database", () -> 0);
     }
 }
